87feaaf4b8db1669a2b8c537803f53527a6b4700
cborre <cborre2007@gmail.com>
2016-04-30 20:59:34 -0430

    GoogleOAuth
    Autenticación con Google OAuth con Passport

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	},
+
+	'googleAuth' : {
+		'clientID': '96862268440-tn89s5jmn8rn876g2gej8mapepe2n126.apps.googleusercontent.com',
+		'clientSecret': 'jKHe2nR5ItQH61IYzwBAGi-L',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
+	}
+}
